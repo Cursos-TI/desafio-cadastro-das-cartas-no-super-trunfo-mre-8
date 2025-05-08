530411c5760ccf322237817ae117095cb68722c0
mre-8 <matheusrabeloeulalio@gmail.com>
2025-05-08 16:10:41 +0000

    comeco mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,9 +8,13 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    int populacaoA01, populacaoA02, pontosturisticosA01, pontosturisticosA02;
-    float pibA01, pibA02, areaA01, areaA02;
-    char cidadeA01[50], cidadeA02[50], estadoA01[50], estadoA02[50], codigoA01[50], codigoA02[50];
+    int pontosturisticosA01, pontosturisticosA02;
+    int multiplicadorinverso = 1;
+    unsigned int populacaoA01, populacaoA02;
+    float areaA01, areaA02, pibA01, pibA02;
+    char cidadeA01[50], cidadeA02[50];
+    char estadoA01[50], estadoA02[50];
+    char codigoA01[50], codigoA02[50];
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -26,8 +30,8 @@ int main() {
     printf("Insira quantidade de pontos turisticos: \n");
     scanf("%i", &pontosturisticosA01);
     printf("insira a populacao da cidade (apenas numeros): \n");
-    scanf("%i", &populacaoA01);
-    printf("Insira o pib da cidade em R$ (apenas numeros): \n");
+    scanf("%u", &populacaoA01);
+    printf("Insira o pib da cidade em R$ (apenas o numero completo): \n");
     scanf("%f", &pibA01);
     printf("Insira o codigo da carta: \n");
     scanf("%s", codigoA01);
@@ -42,22 +46,29 @@ int main() {
     printf("Insira quantidade de pontos turisticos: \n");
     scanf("%i", &pontosturisticosA02);
     printf("insira a populacao da cidade (apenas numeros): \n");
-    scanf("%i", &populacaoA02);
-    printf("Insira o pib da cidade em R$ (apenas numeros): \n");
+    scanf("%u", &populacaoA02);
+    printf("Insira o pib da cidade em R$ (apenas o numero completo): \n");
     scanf("%f", &pibA02);
     printf("Insira o codigo da carta: \n");
     scanf("%s", codigoA02);
     printf("\n");
 
-    float pibpercapitaA01 = (float) pibA01 / populacaoA01, pibpercapitaA02 = (float) pibA02 / populacaoA02;
-    float densidadepopulacaoA01 = (float) populacaoA01 / areaA01, densidadepopulacaoA02 = (float) populacaoA02 / areaA02;
+    float pibpercapitaA01 = (float) pibA01 / populacaoA01;
+    float pibpercapitaA02 = (float) pibA02 / populacaoA02;
+    float densidadepopulacaoA01 = (float) populacaoA01 / areaA01;
+    float densidadepopulacaoA02 = (float) populacaoA02 / areaA02;
+    float inversodensidadeA01 = (float) multiplicadorinverso / densidadepopulacaoA01;
+    float inversodensidadeA02 = (float) multiplicadorinverso / densidadepopulacaoA02;
+    float superpoderA01 = (float) pontosturisticosA01 + populacaoA01 + areaA01 + pibA01 + pibpercapitaA01 + densidadepopulacaoA01 + inversodensidadeA01;
+    float superpoderA02 = (float) pontosturisticosA02 + populacaoA02 + areaA02 + pibA02 + pibpercapitaA02 + densidadepopulacaoA02 + inversodensidadeA02;
+
 
     printf("Carta: %s\n", cidadeA01);
     printf("Estado: %s\n", estadoA01);
     printf("Area em km2: %.2f\n", areaA01);
     printf("Pontos Turisticos: %i\n", pontosturisticosA01);
-    printf("Populacao: %i\n", populacaoA01);
-    printf("Densidade Populacional hab/km2: %.2f \n", densidadepopulacaoA01);
+    printf("Populacao: %u\n", populacaoA01);
+    printf("Densidade Populacional hab/km2: %.2f\n", densidadepopulacaoA01);
     printf("PIB em R$: %.2f\n", pibA01);
     printf("PIB Per Capita em R$: %.2f\n", pibpercapitaA01);
     printf("Codigo da Cidade: %s\n", codigoA01);
@@ -67,8 +78,8 @@ int main() {
     printf("Estado: %s\n", estadoA02);
     printf("Area em km2: %.2f\n", areaA02);
     printf("Pontos Turisticos: %i\n", pontosturisticosA02);
-    printf("Populacao: %i\n", populacaoA02);
-    printf("Densidade Populacional hab/km2: %.2f \n", densidadepopulacaoA02);
+    printf("Populacao: %u\n", populacaoA02);
+    printf("Densidade Populacional hab/km2: %.2f\n", densidadepopulacaoA02);
     printf("PIB em R$: %.2f\n", pibA02);
     printf("PIB Per Capita em R$: %.2f\n", pibpercapitaA02);
     printf("Codigo da Cidade: %s\n", codigoA02);
